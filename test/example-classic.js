835ece4fcd1cfe3d2c7b4f26afbcb83bfc59b047
Colby Fayock <colbyfayock@users.noreply.github.com>
2021-06-09 13:57:24 -0400

    Update example-classic.js

--- test/example-classic.js
+++ test/example-classic.js
@@ -3,7 +3,7 @@ module.exports = {
     'Classic Runner Test': function(browser) {
         browser
             .url('https://demo.applitools.com')
-            .eyesOpen('Demo App', 'Smoke Test', {width: 800, height: 600})
+            .eyesOpen('Demo App - Nightwatch - Classic', 'Smoke Test - Nightwatch - Classic', {width: 800, height: 600})
             .eyesCheck(Target.window().fully().withName('Login Window'))
             .click('#log-in')
             .eyesCheck(Target.window().fully().withName('App Window'))
